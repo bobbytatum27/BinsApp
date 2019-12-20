ce4ec61219a699b2ab71b98531359e44d2934b65
rmtatum <rmtatum@ucsd.edu>
2019-12-20 01:47:25 -0700

    added bins line aka test

--- BinsApp/App.js
+++ BinsApp/App.js
@@ -12,6 +12,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text>Bins Bins Bins Bins Bins</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
