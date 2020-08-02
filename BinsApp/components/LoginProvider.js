6713facd6219bd86e1c9855de2c007e5bedeee15
rmtatum <rmtatum@ucsd.edu>
2020-08-01 17:22:06 -0700

    classed based provider and login state passed via context

--- BinsApp/components/LoginProvider.js
+++ BinsApp/components/LoginProvider.js
@@ -2,11 +2,40 @@ import React, {createContext, useState} from 'react';
 
 // for login,signup context
 export const LoginContext = createContext();
+
+export class LoginProvider extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false,
+            test: 'Test: State passed through.'
+        };
+    }
+
+    login = () => this.setState({isLoggedIn: true});
+    logout = () => this.setState({isLoggedIn: false});
+
+    render() {
+        return (
+            <LoginContext.Provider value={{...this.state, login: this.login, logout: this.logout}}>
+                {this.props.children}
+            </LoginContext.Provider>
+        );
+    }
+}
+
+
+
+/*
+    For reference: this is the original context code in function form
+
 export function LoginProvider(props) {
-  const [isLoggedIn, login] = useState(false);      // to be used for the actual login context value
+  const [isLoggedIn, login] = useState(0);      // to be used for the actual login context value
   return (
-    <LoginContext.Provider value={'test'}>
+    <LoginContext.Provider value={isLoggedIn}>
       {props.children}
     </LoginContext.Provider>
   )
 }
+*/
--- BinsApp/screens/Landing.js
+++ BinsApp/screens/Landing.js
@@ -6,45 +6,45 @@ import { styles } from '../styles/styles.js'
 import {LoginProvider, LoginContext} from '../components/LoginProvider.js'
 
 export default class Landing extends Component {
+  // this is sets Class.context, allows you to access the context via this.context
+  // this.context is set to the state of the LoginProvider
+  static contextType = LoginContext;
+
   render() {
     return (
-      <LoginContext.Consumer>
-        {(context) => (
-          <View style={{flex: 1,
-            backgroundColor: '#261136',
-            padding: 30}}>
-            <View style={{flex:5}}>
-              <Text style={styles.header}>Bins Storage + {context}</Text>
-              <Text style={styles.intro}>We help pick up and bring back items that you'd like to store in self-storage.</Text>
-              <Text style={styles.intro}>Swipe to learn more.</Text>
-            </View>
-            <View style={{flex:5}}>
-              <Text style={styles.intro3}>Discover full-service storage near you</Text>
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('SelectFacilityScreen')}
-                style = {{alignItems: 'center',
-                          backgroundColor: '#E0B141',
-                          padding: 7,
-                          marginTop: 15,
-                          marginLeft: 120,
-                          marginRight: 120,
-                          borderRadius: 20, }} >
-                <Text>Find a Unit</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{flex:3}}>
-              <Text style={{textAlign: 'center',
-                color: 'gray',
-                fontSize: 15,
-                marginTop: 100}}>Returing User?</Text>
-              <Button
-                onPress={() => this.props.navigation.navigate('Login')}
-                title='LOGIN'
-                color= 'white' />
-            </View>
-          </View>
-        )}
-      </LoginContext.Consumer>
+      <View style={{flex: 1,
+        backgroundColor: '#261136',
+        padding: 30}}>
+        <View style={{flex:5}}>
+        <Text style={styles.header}>Bins Storage {this.context.test}</Text>
+          <Text style={styles.intro}>We help pick up and bring back items that you'd like to store in self-storage.</Text>
+          <Text style={styles.intro}>Swipe to learn more.</Text>
+        </View>
+        <View style={{flex:5}}>
+          <Text style={styles.intro3}>Discover full-service storage near you</Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate('SelectFacilityScreen')}
+            style = {{alignItems: 'center',
+                      backgroundColor: '#E0B141',
+                      padding: 7,
+                      marginTop: 15,
+                      marginLeft: 120,
+                      marginRight: 120,
+                      borderRadius: 20, }} >
+            <Text>Find a Unit</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex:3}}>
+          <Text style={{textAlign: 'center',
+            color: 'gray',
+            fontSize: 15,
+            marginTop: 100}}>Returing User?</Text>
+          <Button
+            onPress={() => this.props.navigation.navigate('Login')}
+            title='LOGIN'
+            color= 'white' />
+        </View>
+      </View>
     );
   }
 }
