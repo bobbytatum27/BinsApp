6bca83df6a9d61b2959fffbf58a9a51dcad8c282
rmtatum <rmtatum@ucsd.edu>
2020-08-05 15:27:07 -0700

    removed duplicate imports

--- BinsApp/screens/Account.js
+++ BinsApp/screens/Account.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Alert, ScrollView } from 'react-native';
 import FormInputHandler from '../components/FormInputHandler.js'
 import LongButton from '../components/LongButton.js'
-import {LoginContext} from '../components/LoginProvider.js'
 
 import {Auth} from 'aws-amplify';
 
